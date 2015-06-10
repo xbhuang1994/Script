20943f5c2ee85165edf72c2b1aec840bab430456
xbhuang <xbhuang1994@gmail.com>
2015-06-10 13:24:02 +0800

    提交Unity编辑器工具
    1.自动保存场景工具
    2.批量保存预设工具
    3.制作战斗关卡工具

---
+++ UnityEditorTools/BatchSavePrefab.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class BatchSavePrefab : MonoBehaviour
+{
+
+    [MenuItem("Tools/从选择的物体保存预设")]
+    static public void CreatPrefab()
+    {
+        GameObject[] objs = Selection.gameObjects;
+        var localfolder = SelectLocalFolder();
+        foreach (GameObject obj in objs)
+        {
+            CreatPrefab(obj, localfolder);
+        }
+    }
+    static private string SelectLocalFolder()
+    {
+        //这里要用自己的保存prefab的路径
+        var folderpath = EditorUtility.OpenFolderPanel("Load png Textures of Directory", "Assets/Prefab/", "");
+        //string localpath = "Assets/Prefab/" + obj.name + ".prefab";
+        int assetsIndex = folderpath.IndexOf("Assets");
+        var localpath = folderpath.Substring(assetsIndex, folderpath.Length - assetsIndex);
+        return localpath;
+    }
+
+    [MenuItem("Tools/从选择的物体保存预设", true)]
+    static bool ValidateCreatePrefab()
+    {
+        return Selection.activeGameObject != null;
+    }
+    //暂不开启
+    //[MenuItem("Tools/保存场景中的预设实例")] 
+    public static void SavePrefabAll()
+    {
+        List<GameObject> prefabObjs = new List<GameObject>();
+        //获取当前场景里的所有游戏对象
+        GameObject[] rootObjects = (GameObject[])UnityEngine.Object.FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject go in rootObjects)
+        {
+            //判断是否是预设实例
+            if (PrefabUtility.GetPrefabType(go) == PrefabType.PrefabInstance)
+            {
+                //获取预设根物体保存到List
+                GameObject prefabRoot = PrefabUtility.FindRootGameObjectWithSameParentPrefab(go);
+                if (!prefabObjs.Contains(prefabRoot))
+                {
+                    prefabObjs.Add(prefabRoot);
+                }
+            }
+        }
+        foreach (GameObject go in prefabObjs)
+        {
+            //这里要用自己的保存prefab的路径
+            string localpath = "Assets/Prefab/" + go.name + ".prefab";
+            CreatNew(go, localpath);
+        }
+        //保存
+        AssetDatabase.SaveAssets();
+    }
+
+    static void CreatPrefab(GameObject obj, string localfolder)
+    {
+        var localpath = localfolder + "/" + obj.name + ".prefab";
+        //判断预设资源是否存在
+        if (AssetDatabase.LoadAssetAtPath(localpath, typeof(GameObject)))
+        {
+            if (EditorUtility.DisplayDialog("预设已经存在", "是否重置？", "yes", "no"))
+            {
+                CreatNew(obj, localpath);
+            }
+        }
+        else
+            CreatNew(obj, localpath);
+    }
+
+    static void CreatNew(GameObject go, string path)
+    {
+        Object prefab = PrefabUtility.CreatePrefab(path, go);
+        PrefabUtility.ReplacePrefab(go, prefab, ReplacePrefabOptions.ConnectToPrefab);
+    }
+}
---
+++ UnityEditorTools/BattleZoneBuildInspector.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+//[CanEditMultipleObjects]
+//[CustomPreviewAttribute(typeof(BattleZoneBuild))]
+public class BattleZoneBuildInspector : Editor
+{
+    //#region stage
+    //SerializedProperty normalStage;
+    //SerializedProperty diffStage;
+    //SerializedProperty hellStage;
+    //#endregion
+
+    //SerializedProperty normalZone1;
+    //SerializedProperty normalZone2;
+    //SerializedProperty normalZone3;
+
+    //SerializedProperty diffZone1;
+    //SerializedProperty diffZone2;
+    //SerializedProperty diffZone3;
+
+    //SerializedProperty hellZone1;
+    //SerializedProperty hellZone2;
+    //SerializedProperty hellZone3;
+
+
+
+    //SerializedProperty zoneDiff;
+    //SerializedProperty zonePrefix;
+    //SerializedProperty zoneIndex;
+    //SerializedProperty stageTypeArrayStr;
+
+
+    //private SerializedObject mBattleZoneBuildOS;
+    //private BattleZoneBuild mBattleZoneBuild;
+    //void OnEnable()
+    //{
+    //    normalStage = serializedObject.FindProperty("normalStage");
+    //    diffStage = serializedObject.FindProperty("diffStage");
+    //    hellStage = serializedObject.FindProperty("hellStage");
+
+    //    normalZone1 = serializedObject.FindProperty("normalZone1");
+    //    normalZone2 = serializedObject.FindProperty("normalZone2");
+    //    normalZone3 = serializedObject.FindProperty("normalZone3");
+
+    //    diffZone1 = serializedObject.FindProperty("diffZone1");
+    //    diffZone2 = serializedObject.FindProperty("diffZone2");
+    //    diffZone3 = serializedObject.FindProperty("diffZone3");
+
+    //    hellZone1 = serializedObject.FindProperty("hellZone1");
+    //    hellZone2 = serializedObject.FindProperty("hellZone2");
+    //    hellZone3 = serializedObject.FindProperty("hellZone3");
+
+    //    zoneDiff = serializedObject.FindProperty("zoneDiff");
+    //    zonePrefix = serializedObject.FindProperty("zonePrefix");
+    //    zoneIndex = serializedObject.FindProperty("zoneIndex");
+    //    stageTypeArrayStr = serializedObject.FindProperty("stageTypeArrayStr");
+    //}
+    public override void OnInspectorGUI()
+    {
+        //serializedObject.Update();
+        //DrawStageBox();
+        //DrawNormalZoneBox();
+        //DrawDiffZoneBox();
+        //DrawHellZoneBox();
+        //DrawInput();
+        string str= "1";
+        EditorGUILayout.LabelField("|", str);
+        Debug.Log(1);
+    }
+
+    //private void DrawStageBox()
+    //{
+    //    GUILayout.Label("Stage Prefabs");
+    //    EditorGUILayout.BeginVertical("Box");
+    //    EditorGUILayout.PropertyField(normalStage, new GUIContent("normalStage"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(diffStage, new GUIContent("diffStage"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(hellStage, new GUIContent("hellStage"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.EndVertical();
+    //}
+    //private void DrawNormalZoneBox()
+    //{
+    //    GUILayout.Label("Normal Prefabs");
+    //    EditorGUILayout.BeginVertical("Box");
+    //    EditorGUILayout.PropertyField(normalZone1, new GUIContent("normalZone1"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(normalZone2, new GUIContent("normalZone2"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(normalZone3, new GUIContent("normalZone3"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.EndVertical();
+    //}
+    //private void DrawDiffZoneBox()
+    //{
+    //    GUILayout.Label("Diff Prefabs");
+    //    EditorGUILayout.BeginVertical("Box");
+    //    EditorGUILayout.PropertyField(diffZone1, new GUIContent("diffZone1"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(diffZone2, new GUIContent("diffZone2"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(diffZone3, new GUIContent("diffZone3"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.EndVertical();
+    //}
+    //private void DrawHellZoneBox()
+    //{
+    //    GUILayout.Label("Hell Prefabs");
+    //    EditorGUILayout.BeginVertical("Box");
+    //    EditorGUILayout.PropertyField(hellZone1, new GUIContent("Hell Zone1"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(hellZone2, new GUIContent("Hell Zone2"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(hellZone3, new GUIContent("Hell Zone3"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.EndVertical();
+    //}
+
+
+    //private void DrawInput()
+    //{
+    //    GUILayout.Label("Input");
+    //    EditorGUILayout.BeginVertical("Box");
+    //    EditorGUILayout.PropertyField(zoneDiff, new GUIContent("Zone Difficulty"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(zonePrefix, new GUIContent("Zone Prefix"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(zoneIndex, new GUIContent("Zone Index"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.PropertyField(stageTypeArrayStr, new GUIContent("Stage Array"));
+    //    serializedObject.ApplyModifiedProperties();
+    //    EditorGUILayout.EndVertical();
+    //}
+}
